827b82d6261e747dd69af28c14d500d6131be689
jpforol <jpforol@gmail.com>
2024-08-06 20:52:05 -0300

    add: new model

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.caption("🚀 Uma aplicação de Chatbot com Streamlit com tecnologia Stable 
 st.session_state.option_model = st.selectbox(
     "Selecione o modelo:",
     (
-        "stabilityai/stable-diffusion-xl-base-1.0",
+        "Kvikontent/midjourney-v6stabilityai",
         "stabilityai/stable-diffusion-3-medium-diffusers",
         "alvdansen/littletinies",
     ),
